ecddd9db1025c7e14693f57e68c198e5805a9fdd
Wes <wes@pillarmarketing.com>
2025-02-01 20:03:54 -0800

    no message

---
+++ Buildin1ATest/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0012668",
+      "name": "R0012668",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -2.371335605025518,
+        "pitch": 0.031827506291023155,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8295974649021556,
+          "pitch": 0.2894907403545979,
+          "rotation": 0,
+          "target": "1-r0012667"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0012667",
+      "name": "R0012667",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.6105452503121462,
+        "pitch": -0.6593167063067433,
+        "fov": 1.5991223976577138
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1354310037146362,
+          "pitch": 0.24725753085467517,
+          "rotation": 0,
+          "target": "0-r0012668"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Price St Test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
